c48a9d8b2335723fbd9618cf95640d4d9d717ea4
migs008 <ava_overide@yahoo.com>
2019-02-25 18:53:24 -0800

    Method 2 working.

--- src/com/company/AssignmentTwo.java
+++ src/com/company/AssignmentTwo.java
@@ -3,20 +3,32 @@ package com.company;
 public class AssignmentTwo {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
     }
 
     public static int numElements(int[][] twoDim) {
         int num = - 1;
         if (twoDim != null) {
             num = 0;
-            for (int[] x: twoDim) {
-                if (x != null) {
-                    num += x.length;
+            for (int i = 0; i < twoDim.length; i++) {
+                if (twoDim[i] != null) {
+                    num += twoDim[i].length;
                 }
             }
         }
         return num;
     }
 
+    public static int numElements(int[][][] threeDim) {
+        int num = -1;
+        if (threeDim != null) {
+            num = 0;
+            for (int i = 0; i < threeDim.length; i++) {
+                if(threeDim[i] != null) {
+                    num += numElements(threeDim[i]);
+                }
+            }
+        }
+        return num;
+    }
 }
--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -39,5 +39,29 @@ public class Test {
         System.out.print("Results when all rows are non-null: ");
         System.out.println(hwTwo.numElements(arrTwo));
 
+        System.out.println("\nMethod 2: Test numElements(int[][][]): ");
+        int[][][] arrThree = null;
+        System.out.print("Results when arr = null: ");
+        System.out.println(hwTwo.numElements(arrThree));
+
+        arrThree = new int[][][]{null, null, null, null, null, null};
+        System.out.print("Results when all arrays are null: ");
+        System.out.println(hwTwo.numElements(arrThree));
+
+        arrThree[2] = arrThree[4] = arrTwo;  // each 2D array has 1021 values
+        System.out.print("Results when some arrays are null: ");
+        System.out.println(hwTwo.numElements(arrThree));  // 2042
+
+        arrThree[0] = new int[90][1];   // 90
+        System.out.print("Results when some arrays are null: ");
+        System.out.println(hwTwo.numElements(arrThree));  // 2132
+
+        arrThree[1] = new int[8][2];   // 16
+        arrThree[3] = new int[3][3];    // 9
+        arrThree[5] = new int[10][2];   // 20
+        System.out.print("Results when all arrays are null: ");
+        System.out.println(hwTwo.numElements(arrThree));  // 2177
+
+
     }
 }
